bbf2688bb416cdc91bc293b2dd6672d3de3b31bd
bakemonostan <ehizomokhomion@gmail.com>
2022-10-04 17:16:01 -0700

    created nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,16 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <NavBar />
+    <router-view />
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import NavBar from './components/NavBar.vue';
+export default {
+  components: { NavBar },
+};
+</script>
 
-nav {
-  padding: 30px;
-}
+<style></style>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
---
+++ src/components/BaseModal.vue
@@ -0,0 +1,9 @@
+<template>
+  <div> odal </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
